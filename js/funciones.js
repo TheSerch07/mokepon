fb46c48a316fca4ff5903562cb5683033f7913e1
TheSerch07 <jejog50@gmail.com>
2023-01-11 21:33:18 -0500

    rivales aleatorios

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,6 @@
+const min = 1
+const max = 3
+
 function iniciarJuego() {
     let botonMascota = document.getElementById("boton-mascota")
     botonMascota.addEventListener("click", seleccionarMascota)
@@ -7,15 +10,31 @@ function seleccionarMascota() {
     let input1 = document.getElementById("hipodoge")
     let input2 = document.getElementById("capipepo")
     let input3 = document.getElementById("ratigueya")
+    let spanMascota = document.getElementById("span-mascota")
 
     if (input1.checked) {
-        alert("Haz seleccionado al poderoso Hipodoge")
+        spanMascota.innerHTML = "Hipodoge"
     } else if (input2.checked) {
-        alert("Haz seleccionado al magnifico Capipepo")
+        spanMascota.innerHTML = "Capipepo"
     } else if (input3.checked) {
-        alert("Haz seleccionado a la grandiosa Ratigueya")
+        spanMascota.innerHTML = "Ratigueya"
     } else {
         alert("Selecciona una mascota!")
+    }
+
+    seleccionarMascotaRival()
+}
+
+function seleccionarMascotaRival() {
+    let numeroAleatorio = Math.floor(Math.random() * (max - min + 1) + min)
+    let spanMascotaRival = document.getElementById("span-mascota-rival")
+
+    if (numeroAleatorio === 1) {
+        spanMascotaRival.innerHTML = "Hipodoge"
+    } else if (numeroAleatorio === 2) {
+        spanMascotaRival.innerHTML = "Capipepo"
+    } else {
+        spanMascotaRival.innerHTML = "Ratigueya"
     }
 }
 
