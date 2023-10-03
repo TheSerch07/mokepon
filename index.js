3490ad22184f48c0ca55c626c28ea7375995f872
TheSerch07 <jejog50@gmail.com>
2023-10-02 21:00:14 -0500

    create first route

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hola, mundo!")
+const express = require("express")
+
+const port = 4000
+const app = express();
+
+class Jugador {
+    constructor(nombre, edad, id) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.id = id;
+    }
+}
+
+let jugadores = [];
+
+app.get("/create", (req, res) => {
+    const { nombre, edad } = req.query;
+
+    const id = jugadores.length + 1;
+
+    const jugador = new Jugador(nombre, edad, id);
+    jugadores.push(jugador);
+
+    console.log(jugadores);
+
+    res.send(`Jugador ${jugador.nombre} creado con éxito!`)
+})
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`)
+})
