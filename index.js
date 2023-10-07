a861d2d55a724377507847b67905628d3845f05d
TheSerch07 <jejog50@gmail.com>
2023-10-06 21:25:55 -0500

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require("express")
+const express = require("express");
+const cors = require("cors");
 
 const port = 4000
 const app = express();
+
+app.use(cors());
 
 class Jugador {
     constructor(nombre, edad, id) {
