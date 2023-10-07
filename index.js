eec0fc4bcd737cb666230fd0b7114a620227a3be
TheSerch07 <jejog50@gmail.com>
2023-10-06 21:26:28 -0500

    middleware json

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
-const port = 4000
+const port = 4000;
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
 class Jugador {
