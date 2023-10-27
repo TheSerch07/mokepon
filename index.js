5bee45f3d49832af229d762fbcfe2f1eb8ab6537
TheSerch07 <jejog50@gmail.com>
2023-10-26 22:25:36 -0500

    add

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post("/mokepon/position/:id", (req, res) => {
     const { y } = req.body
 
     const jugadorIndex = jugadores.findIndex((jugador) => jugador.id === id)
-    console.log(jugadorIndex, "??")
+    // console.log(jugadorIndex, "??")
     
     res.end()
 })
